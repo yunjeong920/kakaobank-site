e50693c1168e98cc35c678e2db83d143ba27144e
yunjeong <yunjeong920@naver.com>
2021-06-04 08:23:10 +0900

    pro페이지 완성

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+$(function() {
+  let swiper = new Swiper(".swiper-container", {
+    spaceBetween: 30,
+    centerdSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  });
+});
